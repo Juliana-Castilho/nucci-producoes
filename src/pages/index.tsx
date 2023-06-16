a497c8f818f5deb21fc8d657cf96c68d857e9baf
juliana-castilho <julie.castilho@gmail.com>
2023-06-15 21:22:04 -0300

    add Card and CardImage to pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
+import CardList from '../../public/card/CardList';
+import { Button } from '../components/Button/Button';
 import Card from '../components/Card/Card';
+import styles from '../components/Card/Card.module.css';
+import CardImage from '../components/CardImage/CardImage';
 import Carousel from '../components/Carousel/Carousel';
 
 export default function Home() {
@@ -8,7 +12,35 @@ export default function Home() {
         <Carousel />
       </div>
       <div>
-        <Card />
+        <div className="bg-identity">
+          <div className="container mx-auto py-16 ph-8 max-w-4xl">
+            <a href={'/portfolio'}>
+              <div className="grid lg:grid-cols-3">
+                {CardList.map((card) => {
+                  return (
+                    <Card className={`${styles['card-scale-bg']}`}>
+                      <CardImage
+                        src={card.img}
+                        alt="Portfólio"
+                        height={90}
+                        width={90}
+                      />
+                      <h3 className="py-3.5 text-primary card-title">
+                        {card.title.toUpperCase()}
+                      </h3>
+                      <p className="text-secondary card-description">
+                        {card.text}
+                      </p>
+                      <a className="inline-flex card-btn" href={'/portfolio'}>
+                        <Button>OUVIR</Button>
+                      </a>
+                    </Card>
+                  );
+                })}
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,3 +1,37 @@
+import React from 'react';
+
+import Carousel from '../components/Carousel/Carousel';
+import Card from '../components/Card/Card';
+
+import CardList from '../../public/card/CardList';
+import { Button } from '../components/Button/Button';
+
 export default function Sobre() {
-  return <h1>Página Sobre</h1>
+  return (
+    <>
+      <Carousel />
+      <div className="bg-identity">
+        <div className="container mx-auto py-8 ph-8 max-w-4xl">
+          <h2 className="py-3.5 text-secondary text-center text-xl">
+            {`Portfólio`.toUpperCase()}
+          </h2>
+          <div className="grid lg:grid-cols-3">
+            {CardList.map((card) => {
+              return (
+                <Card>
+                  <h3 className="py-3.5 text-primary card-title">
+                    {card.title.toUpperCase()}
+                  </h3>
+                  <p className="text-secondary card-description">{card.text}</p>
+                  <a className="inline-flex card-btn" href={'/portfolio'}>
+                    <Button>OUVIR</Button>
+                  </a>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
