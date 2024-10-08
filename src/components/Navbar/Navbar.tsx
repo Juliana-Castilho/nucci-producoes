d7a660e66e48c7c420f8f9a7e6fb3711230261e9
juliana-castilho <julie.castilho@gmail.com>
2024-10-07 21:02:42 -0300

    refactor: dropdown menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,9 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import navItems from './navbar.json';
+
 import style from './Navbar.module.css';
 
 export default function Navbar() {
@@ -45,75 +48,44 @@ export default function Navbar() {
                 }`}
               >
                 <ul className="h-screen md:h-max items-baseline justify-center md:flex">
-                  <li
-                    className="pb-6 text-orange-200 py-2 md:px-6 text-center border-b-2 md:border-b-0 border-primary md:hover:text-primary md:hover:bg-transparent"
-                    onClick={() => setNavbar(!navbar)}
-                  >
-                    <Link href="/">Home</Link>
-                  </li>
-                  <li
-                    className="pb-6 text-orange-200 py-2 md:px-6 text-center border-b-2 md:border-b-0 border-primary md:hover:text-primary md:hover:bg-transparent"
-                    onClick={() => setNavbar(!navbar)}
-                  >
-                    <Link href="/sobre">Sobre</Link>
-                  </li>
-                  <li
-                    className="pb-6 text-orange-200 py-2 md:px-6 text-center border-b-2 md:border-b-0 border-primary md:hover:text-primary md:hover:bg-transparent"
-                    onClick={() => setNavbar(!navbar)}
-                  >
-                    <Link href="/portfolio">Portfólio</Link>
-                  </li>
-                  {/* DROPDOWN */}
-                  <li
-                    className="pb-6 text-orange-200 py-2 md:px-6 text-center border-b-2 md:border-b-0 border-primary md:hover:text-primary md:hover:bg-transparent"
-                    onClick={() => setDropdown(!dropdown)}
-                  >
-                    <Link href="#">Serviços&nbsp;▾</Link>
-                    <div
-                      className={`md:absolute w-full md:w-auto md:max-w-7x1 md:right-28 md:text-end ${
-                        dropdown ? 'block' : 'hidden'
-                      }`}
-                    >
-                      <ul
-                        className={`bg-gradient-to-b from-black/30 to-black/10 md:from-black/40 md:to-black/80 mt-4 md:rounded-b-md ${style.dropdownBox}`}
+                  {navItems.map((navItem) => {
+                    return navItem.items ? (
+                      <li
+                        key={navItem.name}
+                        className="pb-6 text-orange-200 py-2 md:px-6 text-center border-b-2 md:border-b-0 border-primary md:hover:text-primary md:hover:bg-transparent"
+                        onClick={() => setDropdown(!dropdown)}
                       >
-                        <li
-                          className="py-3 px-6 border-b-2 border-t-2 md:border-t-0 border-primary/50 md:hover:text-primary md:hover:bg-transparent"
-                          onClick={() => setNavbar(!navbar)}
+                        <Link href="#">{navItem.name}</Link>
+                        <div
+                          className={`md:absolute w-full md:w-auto md:max-w-7x1 md:right-28 md:text-end ${
+                            dropdown ? 'block' : 'hidden'
+                          }`}
                         >
-                          <Link href="/servicos/gravacao">Gravação</Link>
-                        </li>
-                        <li
-                          className="py-3 px-6 border-b-2 border-primary/50 md:hover:text-primary md:hover:bg-transparent"
-                          onClick={() => setNavbar(!navbar)}
-                        >
-                          <Link href="/servicos/mixagem">Mixagem</Link>
-                        </li>
-                        <li
-                          className="py-3 px-6 border-b-2 border-primary/50 md:hover:text-primary md:hover:bg-transparent"
-                          onClick={() => setNavbar(!navbar)}
-                        >
-                          <Link href="/servicos/masterizacao">
-                            Masterização
-                          </Link>
-                        </li>
-                        <li
-                          className="py-3 px-6 border-b-2 border-primary/50 md:hover:text-primary md:hover:bg-transparent"
-                          onClick={() => setNavbar(!navbar)}
-                        >
-                          <Link href="/servicos/producao-musical">
-                            Produção Musical
-                          </Link>
-                        </li>
-                        <li
-                          className="py-3 px-6"
-                          onClick={() => setNavbar(!navbar)}
-                        >
-                          <Link href="/servicos/aulas">Aulas</Link>
-                        </li>
-                      </ul>
-                    </div>
-                  </li>
+                          <ul
+                            className={`bg-gradient-to-b from-black/30 to-black/10 md:from-black/40 md:to-black/80 mt-4 md:rounded-b-md ${style.dropdownBox}`}
+                          >
+                            {navItem.items.map((subItem) => (
+                              <li
+                                key={subItem.name}
+                                className="py-3 px-6 border-b-2 border-primary/50 md:hover:text-primary md:hover:bg-transparent"
+                                onClick={() => setNavbar(!navbar)}
+                              >
+                                <Link href={subItem.href}>{subItem.name}</Link>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      </li>
+                    ) : (
+                      <li
+                        key={navItem.name}
+                        className="pb-6 text-orange-200 py-2 md:px-6 text-center border-b-2 md:border-b-0 border-primary md:hover:text-primary md:hover:bg-transparent"
+                        onClick={() => setNavbar(!navbar)}
+                      >
+                        <Link href={navItem.href}>{navItem.name}</Link>
+                      </li>
+                    );
+                  })}
                 </ul>
               </div>
             </div>
@@ -123,3 +95,4 @@ export default function Navbar() {
     </>
   );
 }
+
